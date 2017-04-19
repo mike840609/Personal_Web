97096d171c33fa3c721baa4bce2dd8cdeb247b12
mike840609 <mike840609@gmail.com>
2017-04-19 20:02:49 +0800

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # heroku
-import os
-if os.getenv('DATABASE_URL') is not None:
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.config()
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
